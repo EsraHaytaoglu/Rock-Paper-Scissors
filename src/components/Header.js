cc3fd0764bc093bec25ca40a36b0c5199cf3e3ac
Esra Cebi <esracebi.81@gmail.com>
2021-10-17 17:21:10 +0300

    gereksiz dosyalar silindi.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Header() {
+    return (
+        <div>
+            <h2>rock <spam>⚡</spam>paper<spam>⚡</spam>scissors</h2>
+        </div>
+    )
+}
+
+export default Header;
